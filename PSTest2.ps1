53f39b11d54fd6999d98c6ee1f075d252e69f9be
My-GitCopilot <ranjeet.batham@hotmail.com>
2025-10-29 20:06:45 +0530

    Remove connection string

--- PSTest2.ps1
+++ PSTest2.ps1
@@ -2,4 +2,4 @@
 #Get-Module SqlServer -ListAvailable
 #Import-Module sqlserver
 # Put the cmdlet and its parameters on the same line so PowerShell parses them correctly.
-Invoke-Sqlcmd -ServerInstance "Batham-PC" -Database "MyAppDb" -Username "sa" -Password "admin@123" -Query "SELECT * FROM Employees" -TrustServerCertificate
+Invoke-Sqlcmd -ServerInstance "localhost" -Database "MyAppDb" -Username "sa" -Password "****" -Query "SELECT * FROM Employees" -TrustServerCertificate
